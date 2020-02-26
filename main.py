151d13988cb8b08a9e598ff1a5c9a54d2c0d6699
Amy <afouzia00@stuy.edu>
2020-02-26 02:40:41 -0500

    main

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ screen = new_screen()
 color = [ 158, 96, 144 ]
 matrix = new_matrix()
 
-for i in range(XRES-1):
+for i in range(XRES):
     add_point(matrix, i, YRES - i)
+    add_point(matrix, YRES - i, i)
 
 draw_lines(matrix, screen, color)
 display(screen)
