3b4787850706b944aa5e97481dc488f6273e3204
Amy <afouzia00@stuy.edu>
2020-02-26 02:50:32 -0500

    main

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ screen = new_screen()
 color = [ 158, 96, 144 ]
 matrix = new_matrix()
 
-add_edge(matrix, 50, 300, 50, 100, 400, 100)
+add_edge(matrix, 50, 350, 50, 100, 400, 100)
+add_edge(matrix, 50, 350, 50, 90, 300, 100)
+add_edge(matrix, 100, 400, 50, 120, 350, 100)
+add_edge(matrix, 90, 300, 50, 120, 350, 100)
 
 draw_lines(matrix, screen, color)
 display(screen)
